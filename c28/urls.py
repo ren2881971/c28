cd3b57e767436074ce468543141349b79a0785a6
Frank Ren <ren2881971@sina.com>
2013-09-17 21:39:33 +0800

    coding in index url

--- c28/urls.py
+++ c28/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import patterns, include, url
-
+from c28.settings import STATIC_URL
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'c28.views.home', name='home'),
-    # url(r'^c28/', include('c28.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('c28.views',
+   url(r'^$','index',name='index'),
 )
