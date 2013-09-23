1e67f7d07d67b3cb415c18dfac76a32910b5e1f2
288 office <ren2881971@sina.com>
2013-09-23 16:31:08 +0800

    account model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+class Account(models.Model):
+    email = models.EmailField(max_length=50)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=32)
+    status = models.IntegerField(max_length=2, default=0)
+    photo = models.CharField(max_length=500)
+    birthday = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __unicode__(self):
+        return self.email
+    class Meta:
+        db_table = 'account'
