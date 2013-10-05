0e9d426be0ee86e41b30132fd1298758152d8e77
clark000 <clark_c@163.com>
2013-10-05 17:52:46 +0800

    forum edit

--- forum/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns = patterns('')
-
-
-urlpatterns += staticfiles_urlpatterns()
