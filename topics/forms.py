bdd5a3d3e997a678f087980a454ac88f0c667bb3
clark000 <clark_c@163.com>
2013-10-09 18:45:33 +0800

    rewrite topics/forms.py

--- topics/forms.py
+++ topics/forms.py
@@ -1,32 +1,18 @@
 #coding:utf-8
-from django import forms
+from django.forms import ModelForm, Textarea, TextInput
 from models import Topic, Topic_reply
 
-class TopicForm(forms.ModelForm):
-	name = forms.CharField(label = u'主题', widget = forms.TextInput(attrs={'size':'80'}))
-	content = forms.CharField(label = u'内容', widget = forms.Textarea(attrs={'cols':'95','rows':'14'}))
-
+class TopicForm(ModelForm):
 	class Meta:
 		model = Topic
+		fields = ('name', 'content')
+		widgets = {
+			'name' : TextInput(attrs = {'size' : 80}),
+			'content' : Textarea(attrs = {'cols' : 80, 'rows' : 8}),
+		}
 
 class NewTopicForm(TopicForm):
-	def save(self):
-#		if not self.name:
-			##TODO:
-		#else:
-		post = Topic(name = self.cleaned_data['name'], content = self.cleaned_data['content'])
-		post.save()
-		return post
+	{}
 
 class EditTopicForm(TopicForm):
-	def __init__(self, *args, **kwargs):
-		super(EditTopicForm, self).__init__(*args, **kwargs)
-		self.initial['name'] = self.instance.name
-		self.initial['content'] = self.instance.content
-
-	def save(self):
-		post = self.instance
-		post.name = self.cleaned_data['name']
-		post.content = self.cleaned_data['content']
-		post.save()
-		return post
+	{}
